42f0274ff661e2c4ee35057a30ae4bbec9b0c670
NostraJames <semaj86@gmail.com>
2019-12-03 00:18:40 -0600

    updating myfunc from HF

--- myfunc.py
+++ myfunc.py
@@ -3,3 +3,10 @@ def myfunc(*args):
         print(a, end='   ')
     if args:
         print()
+
+def nyfunc2(**kwargs):
+    for k, v in kwargs.items():
+        print(k,v,sep='->', end='  ')
+    if kwargs:
+        print()
+
